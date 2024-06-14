0e6d09670b88d89af3eaa4255757181365f5dd17
Jack <liangxiaoming@qing5.com>
2024-06-14 09:00:33 +0800

    * 去掉bufferline 文件图标

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -6,6 +6,7 @@ if present then
             buffer_close_icon = '',
             modified_icon = '●',
             close_icon = '',
+            show_buffer_icons=false,
             close_command = "bdelete! %d", -- 点击关闭按钮关闭
             right_mouse_command = "bdelete! %d", -- 右键点击关闭
             numbers="ordinal",
